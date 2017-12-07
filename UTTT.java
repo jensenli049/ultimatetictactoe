db1fb0424a562eb52202696d7eb152916b52415b
Jensen Li <jensenli049@gmail.com>
2017-12-07 15:56:07 -0500

    fixed printboard

--- UTTT.java
+++ UTTT.java
@@ -5,16 +5,18 @@ public class UTTT{
 	BigSquare board = new BigSquare();
 
 	//testing
+	
 	board.getSquare(2).setSquare(3,'x');
 	board.getSquare(2).setSquare(7,'o');
 	board.getSquare(2).populate();
 	board.printBoard();
-	/*
-	System.out.println("--------------------------------");
+	System.out.println();
+	System.out.println("=====================================================");
+	System.out.println();
 	board.getSquare(2).unpopulate();
 	board.getSquare(5).setSquare(2,'x');
 	board.getSquare(7).setSquare(8,'o');
 	board.printBoard();
-	*/
+
 	}
 }
