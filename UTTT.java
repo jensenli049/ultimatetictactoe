aa61b7ec937f092793bc5e414e7d8c3624ccb262
Jensen Li <jensenli049@gmail.com>
2017-12-11 20:25:39 -0500

    kinda finished the actual game

--- UTTT.java
+++ UTTT.java
@@ -3,42 +3,51 @@ public class UTTT{
     public static void main(String [] args){
 	//initializing
 	BigSquare board = new BigSquare();
-	int index;
+	int currIndex = 0, nextIndex = 0;
 	char cPlayer;
 
 	//run game
 	while(!board.isOver()){
 	    cPlayer = board.getPlayer(); //begins at turn 1
-	    if(board.getTurn() == 1)
-		index = board.freebie();
+	    currIndex = nextIndex;
+	    if( (board.getTurn() == 1) || board.getSquare(currIndex).isOver())
+		currIndex = board.freebie();
+	    nextIndex = board.normalTurn(currIndex);
+	    board.getSquare(currIndex).setSquare(nextIndex,cPlayer);
+	    if(board.getSquare(currIndex).isOver() && !(board.getSquare(currIndex).getIndex(0) == '-')){
+		board.setBBoard(currIndex,cPlayer);
+		board.getSquare(currIndex).setWinner(cPlayer);
+		continue;
+	    }
+	    else
+		board.setBBoard(currIndex,'-');
 	    
-		    
 
 	}	
 
     }  
 }
 /*testing
-	board.getSquare(2).setSquare(3,'x');
-	board.getSquare(2).setSquare(7,'o');
-	board.getSquare(2).setSquare(4,'x');
-	board.getSquare(2).setSquare(1,'o');
-	board.getSquare(2).populate();
-	board.printBoard();
-	System.out.println("User places 'x' in 6");
-	board.getSquare(2).setSquare(5,'x');
-	System.out.println(board.getSquare(2).isOver());
-	board.getSquare(2).setWinner('x');
-	System.out.println();
-	System.out.println("=====================================================");
-	System.out.println();
-	board.getSquare(2).unpopulate();
-	board.getSquare(5).setSquare(2,'x');
-	board.getSquare(7).setSquare(8,'o');
-	board.printBoard();
-	System.out.println("=====================================================");
-	board.getBBoard().printBoard();
+  board.getSquare(2).setSquare(3,'x');
+  board.getSquare(2).setSquare(7,'o');
+  board.getSquare(2).setSquare(4,'x');
+  board.getSquare(2).setSquare(1,'o');
+  board.getSquare(2).populate();
+  board.printBoard();
+  System.out.println("User places 'x' in 6");
+  board.getSquare(2).setSquare(5,'x');
+  System.out.println(board.getSquare(2).isOver());
+  board.getSquare(2).setWinner('x');
+  System.out.println();
+  System.out.println("=====================================================");
+  System.out.println();
+  board.getSquare(2).unpopulate();
+  board.getSquare(5).setSquare(2,'x');
+  board.getSquare(7).setSquare(8,'o');
+  board.printBoard();
+  System.out.println("=====================================================");
+  board.getBBoard().printBoard();
 	
-	System.out.println("=====================================================");
-	System.out.println("Index return: " + board.freebie());
+  System.out.println("=====================================================");
+  System.out.println("Index return: " + board.freebie());
 */
