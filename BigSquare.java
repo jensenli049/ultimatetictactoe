27a4ba4c5eb33e77e4efe4749970e141db771d9b
namreh <hlin9@stuy.edu>
2017-12-12 10:18:47 -0500

    COLOR

--- BigSquare.java
+++ BigSquare.java
@@ -86,12 +86,16 @@ public class BigSquare{
 	}
 	Scanner s = new Scanner(System.in);
 	System.out.print("Pick a square: "); //player picks
-	while(!s.hasNextInt()) s.next();
-	int num = s.nextInt();
-	while (!findVal(unused,num) && !s.hasNextInt()){ //asks until player picks and available square
+	while(!s.hasNextInt()){
 	    System.out.println("Please enter a numbered square!");
 	    System.out.print("Pick a square: ");
 	    s.next();
+	}
+	int num = s.nextInt();
+	while (!findVal(unused,num)){ //asks until player picks and available square
+	    System.out.println("Please enter a numbered square!");
+	    System.out.print("Pick a square: ");
+	    //s.next();
 	    num = s.nextInt();
 	}
 	
@@ -105,37 +109,37 @@ public class BigSquare{
 		return true;
 	return false;
     }
-
+    
     public void printBoard(){
 	String DIVIDER1 = " ---+---+--- | ---+---+--- | ---+---+--- ";
 	String DIVIDER2 = "-------------+-------------+-------------";
 
-	char[] temp = new char[81];
+	String[] temp = new String[81];
 	int counter = 0;
 	for(int i = 0; i < 3; i++) 
 	    for(int j = 0; j < 3; j++) 
 		for(int k = 0; k < 3; k++) 
 		    for(int l = 0; l < 3; l++) {
-			temp[counter] = _STTT[i*3 + k].getIndex(j * 3 + l);
+			temp[counter] = _STTT[i*3 + k].addColor(_STTT[i*3 + k].getIndex(j * 3 + l));
 			counter++;
 		    }
 	
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[9], temp[10], temp[11], temp[12], temp[13], temp[14], temp[15], temp[16], temp[17]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[9], temp[10], temp[11], temp[12], temp[13], temp[14], temp[15], temp[16], temp[17]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[18], temp[19], temp[20], temp[21], temp[22], temp[23], temp[24], temp[25], temp[26]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[18], temp[19], temp[20], temp[21], temp[22], temp[23], temp[24], temp[25], temp[26]);
 	System.out.println(DIVIDER2);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[27], temp[28], temp[29], temp[30], temp[31], temp[32], temp[33], temp[34], temp[35]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[27], temp[28], temp[29], temp[30], temp[31], temp[32], temp[33], temp[34], temp[35]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[36], temp[37], temp[38], temp[39], temp[40], temp[41], temp[42], temp[43], temp[44]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[36], temp[37], temp[38], temp[39], temp[40], temp[41], temp[42], temp[43], temp[44]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[45], temp[46], temp[47], temp[48], temp[49], temp[50], temp[51], temp[52], temp[53]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[45], temp[46], temp[47], temp[48], temp[49], temp[50], temp[51], temp[52], temp[53]);
 	System.out.println(DIVIDER2);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[54], temp[55], temp[56], temp[57], temp[58], temp[59], temp[60], temp[61], temp[62]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[54], temp[55], temp[56], temp[57], temp[58], temp[59], temp[60], temp[61], temp[62]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[63], temp[64], temp[65], temp[66], temp[67], temp[68], temp[69], temp[70], temp[71]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[63], temp[64], temp[65], temp[66], temp[67], temp[68], temp[69], temp[70], temp[71]);
 	System.out.println(DIVIDER1);
-	System.out.printf("  %c | %c | %c  |  %c | %c | %c  |  %c | %c | %c  \n", temp[72], temp[73], temp[74], temp[75], temp[76], temp[77], temp[78], temp[79], temp[80]);
+	System.out.printf("  %s | %s | %s  |  %s | %s | %s  |  %s | %s | %s  \n", temp[72], temp[73], temp[74], temp[75], temp[76], temp[77], temp[78], temp[79], temp[80]);
     }
 }
--- SmallSquare.java
+++ SmallSquare.java
@@ -4,6 +4,7 @@ public class SmallSquare{
     private char _winner;
     private boolean _gameover;
     private int state = 0;
+
     
     public SmallSquare(){
 	_sttt = new char[9];
@@ -34,8 +35,10 @@ public class SmallSquare{
 	   (_sttt[1] == _sttt[4] && _sttt[4] == _sttt[7]) ||
 	   (_sttt[2] == _sttt[5] && _sttt[5] == _sttt[8]) ||
 	   (_sttt[2] == _sttt[4] && _sttt[4] == _sttt[6]) ||
-	   (_sttt[0] == _sttt[4] && _sttt[4] == _sttt[8]))
+	   (_sttt[0] == _sttt[4] && _sttt[4] == _sttt[8])){
+	    //setWinner(winner);
 	    return _gameover = true;
+	}
 	unpopulate();
 	for( char x : _sttt )
 	    if( x == ' ')
@@ -66,16 +69,29 @@ public class SmallSquare{
 		_sttt[i] = ' ';
     }
 
+    public String addColor (char z) {
+	String ANSI_RED = "\u001B[31m";
+	String ANSI_CYN = "\u001B[36m";
+	String ANSI_GRN = "\u001B[33m";
+	String ANSI_RST = "\u001B[0m";
+	
+	if (z == 'x') 
+	    return ANSI_RED + z + ANSI_RST;
+	if (z == 'o')
+	    return ANSI_CYN + z + ANSI_RST;
+	return ANSI_GRN + z + ANSI_RST;
+    }
+
     public void printBoard(){
 	populate();
 	String s1 = "             |             |            \n";
 	String s2 = " ------------+-------------+------------\n";
 	System.out.print(s1+s1);
-	System.out.printf("      %c      |      %c      |      %c      \n",_sttt[0],_sttt[1],_sttt[2]);
+	System.out.printf("      %s      |      %s      |      %s      \n",addColor(_sttt[0]),addColor(_sttt[1]),addColor(_sttt[2]));
 	System.out.print(s1+s1+s2+s1+s1);
-	System.out.printf("      %c      |      %c      |      %c      \n",_sttt[3],_sttt[4],_sttt[5]);
+	System.out.printf("      %s      |      %s      |      %s      \n",addColor(_sttt[3]),addColor(_sttt[4]),addColor(_sttt[5]));
 	System.out.print(s1+s1+s2+s1+s1);
-	System.out.printf("      %c      |      %c      |      %c      \n",_sttt[6],_sttt[7],_sttt[8]);
+	System.out.printf("      %s      |      %s      |      %s      \n",addColor(_sttt[6]),addColor(_sttt[7]),addColor(_sttt[8]));
 	System.out.print(s1+s1);
 	unpopulate();
     }
