2cbf53c84a9bb68a979fbfe6a6a15c61a2e876cb
henrylin <hl239@uw.edu>
2020-01-24 13:37:45 -0800

    added a basic AI and (unfinished) "Henry AI"

--- AI.java
+++ AI.java
@@ -1,46 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.io.*;
 
-public class AI{
-	
-	public AI(){
-		
+public class AI {
+
+	private BigSquare bigBoard;
+	private SmallSquare smallBoard;
+
+	public AI() {
 	}
-	
-	public boolean findVal(int[] arr, int val){
-		for(int x : arr)
-			if(x == val && x != 0)
-				return true;
+
+	public int getBestMoves(SmallSquare tttBoard, int[] availableSpace) {
+		if (1 == 1) {
+			return completeRandomness(tttBoard, availableSpace);
+		}
+		else if (2 == 2) {
+			return winBarely(tttBoard, availableSpace);
+		}
+	}
+
+	public int completeRandomness(SmallSquare tttBoard, int[] availableSpace) {
+		char[] boardState = tttBoard.getBoard();
+		int random = new Random().nextInt(availableSpace.length);
+		return random;
+	}
+
+	public int winBarely(SmallSquare tttBoard, int[] availableSpace) {
+		int number = 0;
+		List<Integer> corners = new ArrayList<>();
+		corners.add(0);
+		corners.add(2);
+		corners.add(6);
+		corners.add(8);
+		if (availableSpace[4] == 0) {
+			number = 4;
+		}
+		else if (getCorners(tttBoard)) {
+			for (int i = 0; i < 4; i++) {
+				if (availableSpace[corners.get(i)] == 0) {
+					number = corners.get(i);
+				}
+			}
+		}
+		else if (canWin(tttBoard)) {
+
+		}
+		else {
+			return number;
+		}
+		return number;
+	}
+
+	public boolean getCorners(SmallSquare tttBoard) {
+		char[] board = tttBoard.getBoard();
+		if (board[0] == '-') {
+			return true;
+		}
 		return false;
 	}
-	
-	public void fileAppend(String str, String filename) throws IOException{
-		BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
-		writer.append(str);
-		writer.close();
+
+	public boolean canWin(SmallSquare tttBoard) {
+		char[] board = tttBoard.getBoard();
+		if (!diagonalWin(board) || verticalWin(board) || horizontalWin(board)) {
+			return false;
+		}
+		return true;
 	}
-	
-	
-	
-	public int bruteForce(int[] unused){
-		
-		return 0;
+
+	public boolean diagonalWin(char[] board) {
+		if (board[0] = "-"
 	}
-	
-	public int randomAI(int[] unused){
-		Random rand = new Random();
-		int n = rand.nextInt(9) + 1;
-		while (!findVal(unused,n))
-			n = rand.nextInt(9) + 1;
-		return n;
+
+	public boolean verticalWin(char[] board) {
+
 	}
-	
-	public int blockingAI(int[] unused){
-		return 0;
+
+	public boolean horizontalWin(char[] board) {
+
 	}
-	
-	public int readAheadAI(int[] unused){
-		return 0;
-	}
-	
 }
